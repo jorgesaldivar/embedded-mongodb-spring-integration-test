5b80be5d7fb026e78855a5bfeb844db63b2184f6
Jorge Saldivar <1724724+jorgesaldivar@users.noreply.github.com>
2019-01-31 22:28:56 -0600

    Grab next available port

--- src/test/java/com/jorgesaldivar/mongodbit/config/MongoConfigTest.java
+++ src/test/java/com/jorgesaldivar/mongodbit/config/MongoConfigTest.java
@@ -14,6 +14,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.util.SocketUtils;
 
 import java.io.IOException;
 
@@ -21,34 +22,34 @@ import java.io.IOException;
 @EnableMongoRepositories(basePackages = "com.jorgesaldivar.mongodbit.repository")
 public class MongoConfigTest {
 
-    @Bean
-    public MongoTemplate mongoTemplate() throws IOException {
-        EmbeddedMongoFactoryBean mongo = new EmbeddedMongoFactoryBean();
-        mongo.setBindIp("localhost");
-        MongoClient mongoClient = mongo.getObject();
-        MongoTemplate mongoTemplate = new MongoTemplate(mongoClient, "embedded_database");
-        return mongoTemplate;
-    }
-
 //    @Bean
 //    public MongoTemplate mongoTemplate() throws IOException {
-//
-//        String bindIp = "localhost";
-//        int port = 12345;
-//
-//        MongodStarter starter = MongodStarter.getDefaultInstance();
-//        IMongodConfig mongodConfig = new MongodConfigBuilder()
-//                .version(Version.Main.PRODUCTION)
-//                .net(new Net(bindIp, port, Network.localhostIsIPv6()))
-//                .build();
-//
-//        MongodExecutable mongodExecutable = starter.prepare(mongodConfig);
-//        MongodProcess mongod = mongodExecutable.start();
-//
-//        MongoClient mongo = new MongoClient(bindIp, port);
-//        MongoTemplate mongoTemplate = new MongoTemplate(mongo, "embedded_database");
+//        EmbeddedMongoFactoryBean mongo = new EmbeddedMongoFactoryBean();
+//        mongo.setBindIp("localhost");
+//        MongoClient mongoClient = mongo.getObject();
+//        MongoTemplate mongoTemplate = new MongoTemplate(mongoClient, "embedded_database");
 //        return mongoTemplate;
-//
 //    }
+
+    @Bean
+    public MongoTemplate mongoTemplate() throws IOException {
+
+        String bindIp = "localhost";
+        int port = SocketUtils.findAvailableTcpPort();
+
+        MongodStarter starter = MongodStarter.getDefaultInstance();
+        IMongodConfig mongodConfig = new MongodConfigBuilder()
+                .version(Version.Main.PRODUCTION)
+                .net(new Net(bindIp, port, Network.localhostIsIPv6()))
+                .build();
+
+        MongodExecutable mongodExecutable = starter.prepare(mongodConfig);
+        MongodProcess mongod = mongodExecutable.start();
+
+        MongoClient mongo = new MongoClient(bindIp, port);
+        MongoTemplate mongoTemplate = new MongoTemplate(mongo, "embedded_database");
+        return mongoTemplate;
+
+    }
 
 }
